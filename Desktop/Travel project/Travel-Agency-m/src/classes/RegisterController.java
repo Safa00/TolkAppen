c1f5f76ec6a64712f6c103681767ab5c0398116e
Safa Al <safa.alkhelfa0076@stud.hkr.se>
2018-03-19 20:42:28 +0100

    modiferad

---
+++ Desktop/Travel project/Travel-Agency-m/src/classes/RegisterController.java
@@ -0,0 +1,106 @@
+package classes;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+//import sun.jvm.hotspot.debugger.AddressException;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import java.io.IOException;
+
+public class RegisterController {
+
+
+    @FXML TextField firstNameField;
+    @FXML TextField lastNameField;
+    @FXML TextField usernameField;
+    @FXML TextField emailField;
+    @FXML TextField passwordField;
+    @FXML TextField confirmPasswordField;
+
+    Register register = new Register();
+
+    DBHandler dbh = new DBHandler();
+
+    public void back(ActionEvent ae) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../fxmlFiles/welcomeScreen.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createAccount(ActionEvent ae){
+
+        dbh.handleUserID();
+        dbh.checkIfUsernameExists();
+
+        int userid = 0 ;
+        userid = userid + Integer.parseInt(Singleton.getInstance().getUserIDnumber());
+        userid++;
+
+        register.setFirstName(firstNameField.getText());
+        register.setLastName(lastNameField.getText());
+        register.setEmail(emailField.getText());
+
+
+        if(Singleton.getInstance().getUsernameList().contains(usernameField.getText())){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Username already exists");
+            alert.showAndWait();
+        } else {
+            register.setUsername(usernameField.getText());
+        }
+
+        if(passwordField.getText().toString().equals(confirmPasswordField.getText().toString())){
+            register.setPassword(confirmPasswordField.getText());
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Strings in the password fields do not match");
+            alert.showAndWait();
+        }
+
+        try {
+            new InternetAddress(emailField.getText().toString()).validate();
+            register.setEmail(emailField.getText().toString());
+        } catch (AddressException ex) {
+            register.setEmail(null);
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("You didn't enter a correct email address");
+            alert.showAndWait();
+        }
+
+        if(register.getFirstName() != null && register.getLastName() != null && register.getUsername() != null && register.getEmail() != null && register.getPassword() != null){
+            dbh.register(Integer.toString(userid), register.getFirstName(), register.getLastName(), register.getUsername(), register.getPassword(), "Normal", register.getEmail());
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Account Created!");
+            alert.setHeaderText("Your account has succesfully been created! You can now log in with your created account!");
+
+            alert.showAndWait();
+
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("../fxmlFiles/welcomeScreen.fxml"));
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+                stage.setScene(scene);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ Desktop/Travel project/Travel-Agency-m/src/classes/WelcomeScreen.java
@@ -0,0 +1,72 @@
+package classes;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WelcomeScreen {
+
+    Register register = new Register();
+
+    public void loginBtnPressed(ActionEvent ae){
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../fxmlFiles/loginScreen.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setResizable(false);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void registerBtnPressed(ActionEvent ae){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../fxmlFiles/registerScreen.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setResizable(false);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        register.setUsername(null);
+        register.setEmail(null);
+        register.setPassword(null);
+        register.setFirstName(null);
+        register.setLastName(null);
+
+    }
+
+    public void helpBtnPressed(ActionEvent ae){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../fxmlFiles/helpScreen.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setResizable(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void quitBtnPressed(){
+        Platform.exit();
+        System.exit(0);
+    }
+}
